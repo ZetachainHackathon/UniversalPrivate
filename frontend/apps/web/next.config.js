7b7db44b57bf833a04f278f254056d653064ee58
Toby1009 <69885352+Toby1009@users.noreply.github.com>
2025-12-19 01:04:01 +0800

    fix: addgit add .

--- frontend/apps/web/next.config.js
+++ frontend/apps/web/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  transpilePackages: ['@st99005912/universal-private-sdk'],
 
   serverExternalPackages: [
     '@railgun-community/wallet',
